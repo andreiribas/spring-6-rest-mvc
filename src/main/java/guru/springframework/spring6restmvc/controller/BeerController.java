2fd5e478c3c490d2fe949e379fc617d85f644c6e
andreiribas <andrei.g.ribas@gmail.com>
2024-10-17 22:25:05 +0200

    Lesson61: Implementation

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -12,23 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.UUID;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 /**
  * Created by jt, Spring Framework Guru.
  */
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = GET)
     public List<Beer> listBeers(){
         log.debug("List Beers - in controller");
         return beerService.listBeers();
     }
 
-    @RequestMapping("/api/v1/beer/{id}")
-    public Beer getBeerById(@PathVariable("id") UUID id){
+    @RequestMapping(value = "/{id}", method = GET)
+    public Beer getBeerById(@PathVariable UUID id){
         log.debug("Get Beer by Id - in controller");
         return beerService.getBeerById(id);
     }
